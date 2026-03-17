1ad2462e0e240a9c3520109e1d5108921a171658
NeoSamyak <1samyaksingh@gmail.com>
2026-03-18 01:13:47 +0530

    created dashboard

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                sage: {
+                    500: '#6B8F7A',
+                    600: '#5A7A68',
+                },
+                'dark-gray': '#3a3a3a',
+            },
+            fontFamily: {
+                serif: ['"Playfair Display"', 'serif'],
+                sans: ['"Inter"', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
